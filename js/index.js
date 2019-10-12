22d9dce9b9472d37a8de527f42d64e2492b6a579
test <593601399@qq.com>
2019-10-12 11:12:14 +0800

    bufenxiugai

--- js/index.js
+++ js/index.js
@@ -4,9 +4,15 @@
         recalc = function(){
             var clientWidth = docEI.clientWidth;
             if(!clientWidth) return;
+
             //100是字体大小，1536是开发时浏览器窗口的宽度，等比计算
             docEI.style.fontSize = 16*(clientWidth/1400)+'px';
+            console.log(clientWidth);
+            if (clientWidth<=2111){
+
+                docEI.style.fontSize='23.5px';
             //console.log(docEI.style.fontSize);
+            }
         }
     if(!doc.addEventListener) return;
     win.addEventListener(resizeEvt, recalc, false);
--- js/lastload.js
+++ js/lastload.js
@@ -1,6 +1,7 @@
 let pointFather = document.getElementById('point');
 let ppFlag = 0;          //痛点选中标志
 let understandFlag = -1;  //了解详情标志
+let dengjiFlag = -1      //等级了解标志
 let talkFlag = 1;
 let pp1 = document.getElementsByClassName('pp1')[0];
 let pp2 = document.getElementsByClassName('pp2')[0];
@@ -15,18 +16,32 @@ let talk1 = document.getElementById('talk1');
 let talk2 = document.getElementById('talk2');
 let talkLength = talk1List.length;
 let index = 0;
+let levelList = document.getElementsByClassName('level');
+let lengthLeve = levelList.length;
+let daohangList = document.getElementsByClassName('daohang')[0].children;
+let daohangLength = daohangList.length;
+let bannerImg = document.getElementsByClassName('banner');
+let divImg = document.getElementsByClassName('bannerdiv');
+
 
 function addEvent() {
     talk1.addEventListener('click',switchTalk);
     talk2.addEventListener('click',switchTalk);
     pp11.addEventListener('click',ppfunction);
     pp12.addEventListener('click',ppfunction);
+    for (let j=0;j<daohangLength;j++){
+        daohangList[j].addEventListener('click',daoFunction);
+    }
+    for (let j=0;j<lengthLeve;j++){
+        levelList[j].addEventListener('click',levelFunction);
+    }
     for (let j=0;j<talkLength;j++){
         talk1List[j].addEventListener('click',switchTalk);
         talk2List[j].addEventListener('click',switchTalk);
     }
     for (let j=0;j<3;j++){
         understandButtom[j].addEventListener('click',unbuttomFunction);
+        divImg[j].addEventListener('click',divIMgFunction);
     }
     for(let j=0;j<i;j++){
         pointList[j].addEventListener('click',dot);}
@@ -34,12 +49,16 @@ function addEvent() {
 addEvent();
 function myRun() {
     run = setInterval(()=>{
+        //console.log(index);
     pointList[index].removeAttribute('id');
-    index++;
+    bannerImg[index].style='display:none';
+        index++;
     if (index==i-1){
         index=0;
     }
+    bannerImg[index].style='display:block';
     pointList[index].setAttribute('id','active');
+    index=index;
     },4000)
 }
 myRun();
@@ -51,7 +70,6 @@ function unbuttomFunction(e){
             let border = document.getElementsByClassName('pain_div')[ev]
             border.style.borderTop='none';
             border.style.borderBottom='0.31rem solid RGB(83, 211, 205)';
-            console.log(border.children);
             for(let j=0;j<4;j++){
                 border.children[j].style="display:block";
             }
@@ -124,6 +142,18 @@ function ppfunction(e) {
         }
     }
 }
+function divIMgFunction(e){
+    clearInterval(run);
+    let event = e.target;
+    let number = Number(event.getAttribute('id').split('bdiv')[1]);
+    pointList[index].removeAttribute('id');
+    bannerImg[index].style='display:none';
+    index = Number(number);
+    pointList[index].setAttribute('id', 'active');
+    bannerImg[index].style='display:block';
+    myRun();
+}
+
 //轮播点点击函数
 function dot(e){
     clearInterval(run);
@@ -131,14 +161,18 @@ function dot(e){
     let ev = event.getAttribute('class').split('p');
     if(ev[1]==0||ev[1]==1){
         pointList[index].removeAttribute('id');
+        bannerImg[index].style='display:none';
         index = Number(ev[1]);
         pointList[index].setAttribute('id', 'active');
+        bannerImg[index].style='display:block';
         myRun();
    }
     else{
         pointList[index].removeAttribute('id');
+        bannerImg[index].style='display:none';
         index = Number(ev[1]);
         pointList[index-1].setAttribute('id', 'active');
+        bannerImg[index-1].style='display:block';
         index = index -1;
         myRun();
     }
@@ -177,4 +211,67 @@ function switchTalk(e){
         talk1List[1].src='./img/白箭头.png';
         talkFlag=1;
     }
+}
+//等级特效
+function levelFunction(e) {
+    let event = e.target;
+    let ev = Number(event.getAttribute('id')[4]);
+    let ture = ev-1;
+    let kuang = document.getElementsByClassName('quan');
+    let deng = document.getElementsByClassName('dengji');
+    let dengco = document.getElementsByClassName('dengco');
+    let quancontent = document.getElementsByClassName('quancontent');
+    if (dengjiFlag==-1){
+        kuang[ture].style.border='0px';
+        deng[ture].style.color='rgb(82, 210, 204)';
+        dengco[ture].style.color='rgb(82, 210, 204)';
+        quancontent[ture].style.backgroundColor='rgb(245, 254, 255)';
+        dengjiFlag = ture;
+    }
+    else{
+        kuang[dengjiFlag].style.border='0.1rem solid rgb(83, 211, 205)';
+        deng[dengjiFlag].style.color='RGB(0, 0, 0)';
+        dengco[dengjiFlag].style.color='RGB(0, 0, 0)';
+        quancontent[dengjiFlag].style.backgroundColor='rgb(255, 255, 255)';
+        kuang[ture].style.border='0px';
+        deng[ture].style.color='rgb(82, 210, 204)';
+        dengco[ture].style.color='rgb(82, 210, 204)';
+        quancontent[ture].style.backgroundColor='rgb(245, 254, 255)';
+        dengjiFlag = ture;
+    }
+}
+function daoFunction(e) {
+    let yuan = document.getElementById('dao_active');
+    let yuanList = document.getElementById('dao_active').children;
+    let yuanIndex = Number(yuanList[2].getAttribute('id').split('dimg')[1])-1;
+    let yuanShowList = document.getElementsByClassName('kuangco')[0].children;
+    console.log(yuanIndex);
+    console.log(yuanShowList);
+    console.log(yuanShowList[yuanIndex]);
+    yuanShowList[yuanIndex].style='display:none';
+    yuan.removeAttribute('id');
+    yuanList[0].removeAttribute('id');
+    yuanList[1].removeAttribute('id');
+    yuanList[2].style='display:none';
+    let event = e.target;
+    if (event.tagName=='DIV'){
+        let eventList = event.children;
+        let xianIndex = Number(eventList[2].getAttribute('id').split('dimg')[1])-1;
+        yuanShowList[xianIndex].style='display:block';
+        event.setAttribute('id','dao_active');
+        eventList[0].setAttribute('id','huobiao');
+        eventList[1].setAttribute('id','huobiaop');
+        eventList[2].style='display:unset';
+    }
+    else{
+        let eventParent = event.parentNode;
+        let eventList = eventParent.children;
+        let xianIndex = Number(eventList[2].getAttribute('id').split('dimg')[1])-1;
+        yuanShowList[xianIndex].style='display:block';
+        eventParent.setAttribute('id','dao_active');
+        eventList[0].setAttribute('id','huobiao');
+        eventList[1].setAttribute('id','huobiaop');
+        eventList[2].style='display:block';
+    }
+
 }
